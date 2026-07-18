cb9fa38bced4cb332defba71d559c7239f2f15eb
Fathi Nashrullah <fnashr@gmail.com>
2026-07-18 10:16:14 +0700

    added schemas

---
+++ backend/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, EmailStr
+from datetime import date as Date
+
+class UserCreate(BaseModel):
+    username:str
+    email:EmailStr
+    password:str
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class WorkoutCreate(BaseModel):
+    day_of_week:str
+    name:str
+    exercises:list[str]
+class WorkoutUpdate(BaseModel):
+    day_of_week:str
+    name:str
+    exercises:list[str]
+class WorkoutResponse(BaseModel):
+    day_of_week:str
+    name:str
+    exercises:list[str]
+
+
+class MarkWorkoutComplete(BaseModel):
+    notes:str | None=None
+class HistoryResponse(BaseModel):
+    date:Date
+    name:str
+    completed:bool
+    notes:str | None
